6e208141f83c121d45050a57b5a62b1ba29080e3
markiankmet <markkmet13@gmail.com>
2023-04-25 21:09:34 +0300

    Add task-6-PR
    Add task-6-PR

---
+++ task-6/Logger.cs
@@ -0,0 +1,11 @@
+﻿namespace Task_6_PR_2._0.Model
+{
+    public class Logger
+    {
+        public Guid Id { get; set; }
+        public string User { get; set; }
+        public string Time { get; set; }
+        public string Action { get; set; }
+        public string Result { get; set; }
+    }
+}
