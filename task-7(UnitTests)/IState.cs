3019d32de1e7b488490c0a2925f9c28dec562b54
markiankmet <markkmet13@gmail.com>
2023-05-10 12:30:36 +0300

    Add task-7-Programing
    Add task-7-Programing

---
+++ task-7(UnitTests)/IState.cs
@@ -0,0 +1,85 @@
+﻿namespace Task7_Programing.Models
+{
+    public interface IState
+    {
+        void Add(Car new_car);
+        void Edit(Car edit_car);
+        void Submit(Car car);
+        void Approve(Car car);
+    }
+    public class DraftState : IState
+    {
+        public void Add(Car new_car)
+        {
+
+        }
+
+        public void Edit(Car edit_car)
+        {
+            edit_car.SetState(new DraftState());
+        }
+
+        public void Submit(Car car)
+        {
+            car.SetState(new ModerationState());
+        }
+
+        public void Approve(Car car)
+        {
+            car.SetState(new PublishedState());
+        }
+        public override string ToString() => StateEnum.Draft.ToString();
+    }
+
+
+    public class ModerationState : IState
+    {
+        public void Add(Car new_car)
+        {
+
+        }
+
+        public void Edit(Car edit_car)
+        {
+
+        }
+
+        public void Submit(Car car)
+        {
+
+        }
+
+        public void Approve(Car car)
+        {
+            car.SetState(new PublishedState());
+        }
+
+        public override string ToString() => StateEnum.Moderation.ToString();
+
+    }
+
+
+    public class PublishedState : IState
+    {
+        public void Add(Car new_car)
+        {
+            new_car.SetState(new DraftState());
+        }
+
+        public void Edit(Car edit_car)
+        {
+            edit_car.SetState(new DraftState());
+        }
+
+        public void Submit(Car car)
+        {
+
+        }
+
+        public void Approve(Car car)
+        {
+
+        }
+        public override string ToString() => StateEnum.Published.ToString();
+    }
+}
